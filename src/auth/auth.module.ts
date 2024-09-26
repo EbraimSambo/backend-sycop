bdc662219c7e1fbfab0fd895ed8468fb6a9e92aa
ebraimsambo <ebraimsambo@gmail.com>
2024-09-26 09:07:48 +0100

    gg

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserReadService } from 'src/user/services/user-read/user-read.service';
+import { UserRepository } from 'src/user/repositories/user.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  providers: [
+    AuthService,
+    UserReadService,
+    UserRepository
+  ],
+  controllers: [AuthController],
+  imports: [
+    JwtModule.register({
+      global: true,
+    }),
+  ]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserReadService } from 'src/user/services/user-read/user-read.service';
+import { LoginDto } from './dtos/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    
+    constructor(
+        private readonly userService: UserReadService
+    ) {}
+
+    async login(values: LoginDto){
+
+        const user = await this.userService.findUserEmail(values.email)
+
+        if(!user) throw new UnauthorizedException("Usuario não encontrado")
+
+            
+    }
+}
