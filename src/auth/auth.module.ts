aa20cdffb94cbdd444661e346e5867cf1aea1912
ebraimsambo <ebraimsambo@gmail.com>
2024-09-26 09:26:35 +0100

    auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,16 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserReadService } from 'src/user/services/user-read/user-read.service';
 import { UserRepository } from 'src/user/repositories/user.repository';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { DatabaseService } from 'src/database/database.service';
 
 @Module({
   providers: [
     AuthService,
     UserReadService,
-    UserRepository
+    UserRepository,
+    JwtService,
+    DatabaseService
   ],
   controllers: [AuthController],
   imports: []
